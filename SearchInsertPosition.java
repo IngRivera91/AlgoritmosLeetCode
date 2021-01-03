d6f8b53642afb13e746075bebaf6cd2437409efd
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-01-02 18:42:07 -0600

    .

---
+++ SearchInsertPosition.java
@@ -0,0 +1,10 @@
+public class SearchInsertPosition {
+
+    public static void main (String [] args) {
+
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        return -1;
+    }
+}
